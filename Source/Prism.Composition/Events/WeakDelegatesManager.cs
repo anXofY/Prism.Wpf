e2be05078c19dac06ef78e63846c057eadac0b40
Brian Lagunas <brianlagunas@users.noreply.github.com>
2015-02-03 12:53:05 -0700

    Added DelegateReference class which removed dependency on PubSubEvents. Also moved WeakDelegatesManager from Regions into Events folder.

--- Source/Prism.Composition/Events/WeakDelegatesManager.cs
+++ Source/Prism.Composition/Events/WeakDelegatesManager.cs
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
-using Microsoft.Practices.Prism.PubSubEvents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
